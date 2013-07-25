2667ede6941dca8d01d1dd8b4f5a4d9163f5b89f
Pascal van Eck <pascal@pascalvaneck.com>
2013-07-25 11:06:31 +0200

    Started to use Grunt for build automation (JSHint, JSDoc, running tests with Karma).

---
+++ Gruntfile.js
@@ -0,0 +1,50 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON("package.json"),
+        jshint: {
+            files: ["Gruntfile.js", "js/**/*.js", "tests/jasmine-specs/*.js"],
+            options: {
+                camelcase: true,
+                immed: true,
+                latedef: true,
+                newcap: true,
+                nonew: true,
+                quotmark: true,
+                trailing: true,
+                maxparams: 5,
+                maxstatements: 50,
+                maxdepth: 4,
+                maxlen: 80
+            }
+        },
+        watch: {
+            files: ["<%= jshint.files %>"],
+            tasks : ["jshint", "qunit"]
+        },
+        karma: {
+            unit: {
+              configFile: "karma.conf.js",
+              singleRun: true,
+              browsers: ["PhantomJS"]
+            }
+        },
+        jsdoc: {
+            dist: {
+                src: ["js/**/*.js", "tests/jasmine-specs/*.js", "README.md"],
+                options: {
+                    destination: "doc"
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks("grunt-contrib-jshint");
+    grunt.loadNpmTasks("grunt-contrib-watch");
+    grunt.loadNpmTasks("grunt-karma");
+    grunt.loadNpmTasks("grunt-jsdoc");
+
+    grunt.registerTask("test", ["jshint"]);
+
+    grunt.registerTask("default", ["jshint", "karma", "jsdoc"]);
+
+};
